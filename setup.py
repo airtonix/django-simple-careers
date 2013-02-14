526e65104aaf5bbcdf695bbe3cfca21d6ab2f766
Zenobius Jiricek <zeno@zibmedia.com.au>
2013-02-14 15:11:38 +1030

    Update setup to properly include license and template discovery

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
+import os
 from setuptools import setup, find_packages
 
+import simplecareers
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
-		name ="simplecareers",
-        version  ="0.1",
-        packages = find_packages(),
-        install_requires = [
-        	"django-uni-form",
-        	"django-classy-tags",
-        	"surlex",
-        	"south",
-        ],
+	name ="django-simple-careers",
+    version=simplecareers.__version__,
+    classifiers = (
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ),
+    packages=find_packages(),
+    install_requires = [
+    	"django-uni-form",
+    	"django-classy-tags",
+    	"surlex",
+    	"south",
+    ),
+    author='Zenobius Jiricek',
+    author_email='airtonix@gmail.com',
+    description='a simple careers application for django. post jobs, toggle them as vacancies and collect applications with resume uploads',
+    long_description = read('README.md'),
+    license='MIT',
+    keywords='django, careers, jobs, vacancies, human resource',
+    url='http://github.com/airtonix/django-simple-careers/',
+    include_package_data=True,
 )
